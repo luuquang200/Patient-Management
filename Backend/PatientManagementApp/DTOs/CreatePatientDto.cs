b0c2faa51034b3e93b4b7097cb6ad2105e3bbe67
quangluu <luungocquang2002@gmail.com>
2024-08-05 21:08:56 +0700

    feat: Added validation rules for the UpdatePatientDto and AddPatientDto

--- Backend/PatientManagementApp/DTOs/CreatePatientDto.cs
+++ Backend/PatientManagementApp/DTOs/CreatePatientDto.cs
@@ -1,4 +1,6 @@
-﻿namespace PatientManagementApp.DTOs
+﻿using FluentValidation;
+
+namespace PatientManagementApp.DTOs
 {
 	public class CreatePatientDto
 	{
@@ -11,4 +13,109 @@
 		public AddressDto? SecondaryAddress { get; set; }
 	}
 
+	public class CreatePatientDtoValidator : AbstractValidator<CreatePatientDto>
+	{
+		public CreatePatientDtoValidator()
+		{
+			RuleFor(x => x.FirstName)
+				.NotEmpty().WithMessage("First name is required.")
+				.MaximumLength(50).WithMessage("First name must not exceed 50 characters.");
+
+			RuleFor(x => x.LastName)
+				.NotEmpty().WithMessage("Last name is required.")
+				.MaximumLength(50).WithMessage("Last name must not exceed 50 characters.");
+
+			RuleFor(x => x.Gender)
+				.NotEmpty().WithMessage("Gender is required.")
+				.Must(g => g == "Male" || g == "Female" || g == "Other")
+				.WithMessage("Gender must be either Male, Female, or Other.");
+
+			RuleFor(x => x.DateOfBirth)
+				.NotEmpty().WithMessage("Date of birth is required.")
+				.LessThan(DateTime.Now).WithMessage("Date of birth must be in the past.");
+
+			RuleFor(x => x.ContactInfos)
+				.NotEmpty().WithMessage("At least one contact info is required.")
+				.Must(c => c.Count > 0).WithMessage("At least one contact info is required.");
+
+			RuleForEach(x => x.ContactInfos).SetValidator(new ContactInfoDtoValidator());
+
+			RuleFor(x => x.PrimaryAddress)
+				.NotNull().WithMessage("Primary address is required.")
+				.SetValidator(new AddressDtoValidator());
+
+			RuleFor(x => x.SecondaryAddress)
+				.SetValidator(new NullableAddressDtoValidator())
+				.When(x => x.SecondaryAddress != null);
+		}
+	}
+
+	public class ContactInfoDtoValidator : AbstractValidator<ContactInfoDto>
+	{
+		public ContactInfoDtoValidator()
+		{
+			RuleFor(x => x.Type)
+				.NotEmpty().WithMessage("Contact type is required.")
+				.MaximumLength(20).WithMessage("Contact type must not exceed 20 characters.");
+
+			RuleFor(x => x.Value)
+				.NotEmpty().WithMessage("Contact value is required.")
+				.MaximumLength(50).WithMessage("Contact value must not exceed 50 characters.");
+		}
+	}
+
+	public class AddressDtoValidator : AbstractValidator<AddressDto>
+	{
+		public AddressDtoValidator()
+		{
+			RuleFor(x => x.Street)
+				.NotEmpty().WithMessage("Street is required.")
+				.MaximumLength(100).WithMessage("Street must not exceed 100 characters.");
+
+			RuleFor(x => x.City)
+				.NotEmpty().WithMessage("City is required.")
+				.MaximumLength(50).WithMessage("City must not exceed 50 characters.");
+
+			RuleFor(x => x.State)
+				.NotEmpty().WithMessage("State is required.")
+				.MaximumLength(50).WithMessage("State must not exceed 50 characters.");
+
+			RuleFor(x => x.ZipCode)
+				.NotEmpty().WithMessage("Zip code is required.")
+				.MaximumLength(20).WithMessage("Zip code must not exceed 20 characters.");
+
+			RuleFor(x => x.Country)
+				.NotEmpty().WithMessage("Country is required.")
+				.MaximumLength(50).WithMessage("Country must not exceed 50 characters.");
+		}
+	}
+
+	public class NullableAddressDtoValidator : AbstractValidator<AddressDto?>
+	{
+		public NullableAddressDtoValidator()
+		{
+			When(x => x != null, () =>
+			{
+				RuleFor(x => x.Street)
+					.NotEmpty().WithMessage("Street is required.")
+					.MaximumLength(100).WithMessage("Street must not exceed 100 characters.");
+
+				RuleFor(x => x.City)
+					.NotEmpty().WithMessage("City is required.")
+					.MaximumLength(50).WithMessage("City must not exceed 50 characters.");
+
+				RuleFor(x => x.State)
+					.NotEmpty().WithMessage("State is required.")
+					.MaximumLength(50).WithMessage("State must not exceed 50 characters.");
+
+				RuleFor(x => x.ZipCode)
+					.NotEmpty().WithMessage("Zip code is required.")
+					.MaximumLength(20).WithMessage("Zip code must not exceed 20 characters.");
+
+				RuleFor(x => x.Country)
+					.NotEmpty().WithMessage("Country is required.")
+					.MaximumLength(50).WithMessage("Country must not exceed 50 characters.");
+			});
+		}
+	}
 }
--- Backend/PatientManagementApp/DTOs/UpdatePatientDto.cs
+++ Backend/PatientManagementApp/DTOs/UpdatePatientDto.cs
@@ -1,7 +1,21 @@
-﻿namespace PatientManagementApp.DTOs
+﻿using FluentValidation;
+
+namespace PatientManagementApp.DTOs
 {
 	public class UpdatePatientDto : CreatePatientDto
 	{
 		public Guid Id { get; set; }
+	}
+
+	public class UpdatePatientDtoValidator : AbstractValidator<UpdatePatientDto>
+	{
+		public UpdatePatientDtoValidator()
+		{
+			// Reuse the CreatePatientDtoValidator rules
+			Include(new CreatePatientDtoValidator());
+
+			// Additional validation rules for UpdatePatientDto
+			RuleFor(x => x.Id).NotEmpty().WithMessage("Id is required.");
+		}
 	}
 }
